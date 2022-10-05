554772dd63d54e2f06d04c395c1afc97ec3fe568
Tchisom17 <53842051+Tchisom17@users.noreply.github.com>
2022-10-06 00:53:11 +0100

    Feat: refactor code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    private float spawnRate = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() { }
+
+    //SpawnTarget is used to spawn objects randomly
+    IEnumerator SpawnTarget()
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+
+            Instantiate(targets[index]);
+        }
     }
 }
